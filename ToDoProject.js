27520e19fa81e0eaabac9a56f04fd6a4cb687372
bulobr01 <bulobr01@luther.edu>
2015-10-19 12:34:09 -0500

    Update ToDoProject.js

--- ToDoProject.js
+++ ToDoProject.js
@@ -1,3 +1,9 @@
 changeText= function() {
-  
+   t = document.querySelector("#list")
+ trow = document.createElement("tr")
+  td = document.createElement("td")
+   contents = document.createTextNode(Text())
+  td.appendChild(contents)
+ trow.appendChild(td)
+  t.appendChild(trow)
 }
