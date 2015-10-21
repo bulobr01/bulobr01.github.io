a6afcd4f9220ea8c788b2dd0c373918c9fe415d7
bulobr01 <bulobr01@luther.edu>
2015-10-21 10:22:49 -0500

    Update ToDoProject.js

--- ToDoProject.js
+++ ToDoProject.js
@@ -8,7 +8,7 @@ var litext = document.createTextNode(document.querySelector("#textbox").value);
   li.appendChild(litext);
   li.className= document.querySelector("#priority").value;
   myUl.appendChild(Myli);
-  document.body.appendChild(li);
+  document.ul.appendChild(li);
  
  
  
