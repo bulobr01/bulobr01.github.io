9db2ef9a145cf7de9b5a1760eba1ca8ce852b145
bulobr01 <bulobr01@luther.edu>
2015-10-21 13:55:47 -0500

    Update ToDoProject.js

--- ToDoProject.js
+++ ToDoProject.js
@@ -3,8 +3,8 @@ moveText= function() {
 var litext = document.createTextNode(document.querySelector("#textbox").value);
  var li= document.createElement("li");
  var cbox= document.createElement("input");
- Ul = document.querySelector("#tasklist");
- Ul.appendChild(li);
+ UL = document.querySelector("#tasklist");
+ UL.appendChild(li);
  cbox.type="checkbox";
   li.appendChild(cbox);
   li.appendChild(litext);
