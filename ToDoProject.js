5ae6606ed12f972ffbbef81c213758365b31094a
bulobr01 <bulobr01@luther.edu>
2015-10-22 22:09:01 -0500

    Update ToDoProject.js

--- ToDoProject.js
+++ ToDoProject.js
@@ -9,7 +9,7 @@ listitem.appendChild(litext);
 //listitem.className = priority;
 listitem.className = document.querySelector('#priority').value;
 cbox.onclick = completeTask;
-var ulsection1 = document.querySelector('#tasklist');
+var ulsection1 = document.querySelector('#willis');
 ulsection1.appendChild(listitem);
 localSave("unlist");
 
