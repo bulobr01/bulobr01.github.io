58c8c5a48072e9becb35e39b4f8f7f2461331998
bulobr01 <bulobr01@luther.edu>
2015-10-22 21:33:17 -0500

    Update ToDoProject.js

--- ToDoProject.js
+++ ToDoProject.js
@@ -1,24 +1,37 @@
+addTask = function() {
+//var priority = document.querySelector('#priority').option.className;
+var litext = document.createTextNode(document.querySelector('#textbox').value);
+var listitem = document.createElement("li");
+var cbox = document.createElement("input");
+cbox.type = "checkbox";
+listitem.appendChild(cbox);
+listitem.appendChild(litext);
+//listitem.className = priority;
+listitem.className = document.querySelector('#priority').value;
+cbox.onclick = completeTask;
+var ulsection1 = document.querySelector('#unlist');
+ulsection1.appendChild(listitem);
+localSave("unlist");
 
-moveText= function() {
-var litext = document.createTextNode(document.querySelector("#textbox").value);
- var li= document.createElement("li");
-
- var cbox= document.createElement("input");
- var menu= document.createElement("select");
 
 
- cbox.type="checkbox";
-  li.appendChild(cbox);
-  li.appendChild(litext);
-  
- UL = document.querySelector("#tasklist");
- UL.appendChild(li);
-  document.body.ul.appendChild(li);
-  
-   menu= document.querySelector("#priority”);
-  li.classList.add(menu.value);
-  
- 
- 
- 
+
 }
+completeTask = function(){
+  if (this.checked) { 
+  this.parentNode.classList.add("done");
+  }
+  else {
+  this.parentNode.classList.remove("done");
+  }
+  localSave("unlist");
+  
+}
+window.onload = function(){
+  restoreList("unlist", completeTask);
+}
+/*window.onload = function(){
+  addTask();
+  completeTask();
+}
+*/
